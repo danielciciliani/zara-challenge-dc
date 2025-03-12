ee3e9a50a68188f95caaafa039d42b93552f20dc
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-03-12 19:01:54 +0100

    fix: correct import path for component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './components/PhoneGrid/PhoneGrid.scss'
-import PhoneGrid from './components/PhoneGrid/PhoneGrid.jsx'
+import PhoneGrid from './components/phoneGrid/phoneGrid'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
