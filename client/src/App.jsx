9c92f6bddb949e7bbcb26300ca59f7906c572174
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-03-14 10:21:39 +0100

    refactor: move component from main to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import PhoneGrid from './components/phoneGrid/phoneGrid'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <PhoneGrid />
     </>
   )
 }
