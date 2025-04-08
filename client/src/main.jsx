0ab22c8b1bc35c2afff7a4896e2eae6dda24a971
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-04-08 10:28:27 +0200

    feat: add routes to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route} from 'react-router'
+import App from './App'
 import './index.css'
 import './components/PhoneGrid/PhoneGrid.scss'
-import App from './App'
+import PhoneDetail from './components/PhoneDetail/PhoneDetail'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<App />} />  
+          <Route path='/phones/:id' element = {<PhoneDetail />} />
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
