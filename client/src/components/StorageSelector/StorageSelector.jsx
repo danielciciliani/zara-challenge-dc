2c6f6e761842c694c9f64c439c7c6fb0e2231546
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-04-16 17:39:09 +0200

    feat: create storageSelector component

--- client/src/components/StorageSelector/StorageSelector.jsx
+++ client/src/components/StorageSelector/StorageSelector.jsx
@@ -5,18 +5,20 @@ import "./StorageSelector.scss";
 function StorageSelector( {storageOptions, selectedStorage, onStorageChange}){
 
     return (
-        <ul className="storage_selector_wrapper">
-            {storageOptions.map((storage) => (
-                <li className={`storage_selector_option ${ 
-                    selectedStorage === storage.capacity ? "selected" : ""
-                }`}
-                    key={storage.capacity}
-                    onClick={() => onStorageChange(storage)}
-                    >
-                    {storage.capacity}
-                </li>
-            ))}
-        </ul>
+        <>
+            <p className="storage_text to_upper">Storage ¿hOW MUCH SPACE DO YOU NEED?</p>
+            <ul className="storage_selector_wrapper">
+                {storageOptions.map((storage) => (
+                    <li  key={storage.capacity} className={`storage_selector_option ${ 
+                        selectedStorage === storage.capacity ? "selected" : ""
+                    }`}
+                        onClick={() => onStorageChange(storage)}
+                        >
+                        {storage.capacity}
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
 
