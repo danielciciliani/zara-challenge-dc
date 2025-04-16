5f79abffb91ff3e6c9fc1488c46e3fe5550eb1f5
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-04-16 17:44:12 +0200

    feat: add funcionality to colorSelector

--- client/src/components/ColorSelector/ColorSelector.jsx
+++ client/src/components/ColorSelector/ColorSelector.jsx
@@ -2,18 +2,29 @@ import PhoneDetail from "../PhoneDetail/PhoneDetail";
 
 import "./ColorSelector.scss";
 
-function ColorSelector( {colorOptions} ) {
+function ColorSelector( {colorOptions, selectedColor, onColorChange} ) {
 
     return (
-        <ul className="color_selector_wrapper">
-            {colorOptions?.map((color) => (
-            <div 
-                className="color_selector_option" 
-                style={{backgroundColor: color.hexCode}}
-                key={color.hexCode}>
-            </div>
-            ))}
-        </ul>
+        <>
+            <p className="storage_text to_upper">color. pick your favourite.</p>
+            <ul className="color_selector_wrapper">
+                {colorOptions?.map((color) => (
+                <div className={`color_selector_border ${
+                    selectedColor === color.hexCode ? "selected" : ""
+                }`}
+                key={color.hexCode}
+                >
+                    <li 
+                        className="color_selector_option"
+                        style={{backgroundColor: color.hexCode}}
+                        onClick={() => onColorChange(color.hexCode)}
+                    >
+
+                    </li>
+                </div> 
+                ))}
+            </ul>
+        </>
     );
 }
 
