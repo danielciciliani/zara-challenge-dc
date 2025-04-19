a4999e52df98aa2ad7a04b2f88cb0d36db1cb639
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-04-19 16:39:28 +0200

    style: add hover effect to PhoneCard component

--- client/src/components/PhoneCard/PhoneCard.jsx
+++ client/src/components/PhoneCard/PhoneCard.jsx
@@ -14,14 +14,15 @@ function PhoneCard ({product}) {
             <div className='grid_card_wrapper'>
                 <img className='grid_card_image' src={product.imageUrl} alt={product.name} />
                 <div className="grid_card_info">
-                    <div className="grid_card_info_brand">
-                        <p className='grid_card_brand'>{product.brand}</p>
-                        <h2 className='grid_card_name'>{product.name}</h2>
+                    <div className="grid_card_info_wrapper">
+                        <p className='grid_card_info_wrapper_brand'>{product.brand}</p>
+                        <h2 className='grid_card_info_wrapper_name'>{product.name}</h2>
                     </div>
                 <p className='grid_card_price'>{product.basePrice} €</p>
                 </div>
             </div>
             </Link >
+            <div className="grid_card_overlay"></div>
         </div>
         </>
     );
