b143c284586b4c916486cd60f188b616e7456253
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-04-23 09:15:23 +0200

    style: add styls to Cart component

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -15,6 +15,23 @@ function Cart() {
             <div className="app_container">
                 <div className="cart">
                     <p className="cart_title">Cart ({addedProducts})</p>
+                    <div className="cart_phone-card_wrapper">
+                        <div className="cart_phone-card">
+                            <div className="cart_phone-card_image">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="cart_phone-card_info">
+                                <div className="cart_phone-card_info_wrapper">
+                                    <p className="name">galaxy s24 ultra</p>
+                                    <p className="data">512 gb  | violeta titanium</p>
+                                    <p className="price">1199 EUR</p>
+                                </div>
+                                <div className="cart_phone-card_delete">
+                                    <p className="delete_button">Eliminar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="cart_footer">
                         <div className="cart_footer_continue">
                             <ContinueShopping />
