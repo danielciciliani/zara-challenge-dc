91abb0acd1fe1fbb244dfce48aa8bfe22bb0d1c5
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-04-24 12:48:58 +0200

    chore: format code with prettier

--- client/src/components/AddToCart/AddToCart.test.jsx
+++ client/src/components/AddToCart/AddToCart.test.jsx
@@ -4,26 +4,38 @@ import AddToCart from './AddToCart';
 
 describe('AddToCart', () => {
   it('renders text in button "Añadir"', () => {
-    render(<AddToCart isActive={true} addToCart={() => {}} addedProducts={[]} />);
+    render(
+      <AddToCart isActive={true} addToCart={() => {}} addedProducts={[]} />,
+    );
     const button = screen.getByText('Añadir');
     expect(button).toBeDefined();
   });
 
   it('applies the active class the isActive is true', () => {
-    render(<AddToCart isActive={true} addToCart={() => {}} addedProducts={[]} />);
+    render(
+      <AddToCart isActive={true} addToCart={() => {}} addedProducts={[]} />,
+    );
     const button = screen.getByText('Añadir');
     expect(button.className).toContain('active');
   });
 
   it('applies the inactive class the isActive is false', () => {
-    render(<AddToCart isActive={false} addToCart={() => {}} addedProducts={[]} />);
+    render(
+      <AddToCart isActive={false} addToCart={() => {}} addedProducts={[]} />,
+    );
     const button = screen.getByText('Añadir');
     expect(button.className).toContain('inactive');
   });
 
   it('calls addToCart when clicked', () => {
-    const mockAddToCart = vi.fn(); // vi.fn() de Vitest para mocks
-    render(<AddToCart isActive={true} addToCart={mockAddToCart} addedProducts={[]} />);
+    const mockAddToCart = vi.fn();
+    render(
+      <AddToCart
+        isActive={true}
+        addToCart={mockAddToCart}
+        addedProducts={[]}
+      />,
+    );
     const button = screen.getByText('Añadir');
     fireEvent.click(button);
     expect(mockAddToCart).toHaveBeenCalledTimes(1);
--- client/src/components/TopBar/TopBar.test.jsx
+++ client/src/components/TopBar/TopBar.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent, act } from '@testing-library/react';
-import { describe, it, expect, vi} from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import TopBar from './TopBar';
 import { MemoryRouter } from 'react-router';
 import { useCartContext } from '../../context/CartContext';
@@ -10,14 +10,12 @@ vi.mock('../../context/CartContext', () => ({
   }),
 }));
 
-
 describe('TopBar', () => {
-
   it('render logo in topbar', () => {
     render(
       <MemoryRouter>
         <TopBar />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     const logoIcon = screen.getByAltText('logo MBST');
     expect(logoIcon).toBeDefined();
@@ -25,15 +23,13 @@ describe('TopBar', () => {
   });
 
   it('render bag icon filled with products added', () => {
-
     render(
       <MemoryRouter>
         <TopBar />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     const bagIcon = screen.getByAltText('bag icon');
     expect(bagIcon).toBeDefined();
     expect(bagIcon.getAttribute('src')).toBe('/bag_icon_filled.svg');
   });
-
 });
